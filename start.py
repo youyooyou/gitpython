d2764b5fa9469fa817ba73813e82c51b48462533
youyooyou <youyooyou@126.com>
2018-09-23 20:37:32 +0800

    工程入口函数

---
+++ start.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time     : 2018/9/23 20:36
+# @Author   : youyooyou@126.com
+# @File     : start.py
+# @Software : PyCharm
+# @ModifyLog:
+
+
+def _test():
+    #TODO
+    pass
+
+
+if __name__ == '__main__':
+    _test()
